3d82be2c3cfe25a6b1dbc655a492c76dbf60e545
brambr <vladimir.manita@gmail.com>
2020-04-03 01:26:53 +0300

    Lesson1

--- Projects/Lesson1/HelloWorld.java
+++ Projects/Lesson1/HelloWorld.java
@@ -1,7 +1,17 @@
 public class HelloWorld{
 	public static void main(String[] arg) {
-		System.out.println ("Hello , World");
-	
+		int m,n;
+		String test_result;
+		m = 112;
+		n = 23;
+		n = n+m;
+		System.out.print("n+m = " );
+		System.out.println(n);
+	// test
+		test_result = "no";
+		if (n ==139) test_result = "OK";
+		System.out.println ("Test" + test_result);
+		
 	}
 
 
